3d75216ebd54add4f4fdf7facf36422a97b6525f
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-07-12 07:58:41 -0700

    Update fcct-create-ign.sh

--- fcct-create-ign.sh
+++ fcct-create-ign.sh
@@ -4,12 +4,11 @@
 # converts the ignition/*.yaml files into .ign
 podman --version || { echo 'podman  --version failed' ; exit 1; }
 ssh-keygen -b 2048 -t rsa -f ssh.key -q -N "" || { echo 'ssh-keygen  failed' ; exit 1; }
-PUB_KEY=$(cat ssh.key.pub)
-echo $PUB_KEY
-sed -e s"|ssh-rsa\ veryLongRSAPublicKey|$PUB_KEY|" -i ignition/lb.yaml
-grep rsa ignition/lb.yaml
 
-# put the public key in the yaml
-exit 1
+# put the key into the yaml files
+PUB_KEY=$(cat ssh.key.pub)
+sed -e s"|ssh-rsa\ veryLongRSAPublicKey|$PUB_KEY|" -i ignition/lb.yaml
+
 podman pull quay.io/coreos/fcct:release
 podman run --rm -v ./ignition/lb.yaml:/config.fcc:z quay.io/coreos/fcct:release --pretty --strict /config.fcc > ignition/lb.ign
+cat ignition/lb.ign
